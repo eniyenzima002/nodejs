fdedce26eb8bfe8f26b263d7e9bf43673918d53a
emmanuelcode <eniyenzima@gmail.com>
2023-02-01 01:26:58 -0600

    few changes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,79 @@
 //import { v4 as uuidv4 } from 'uuid';
-const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const mongoose = require('mongoose');
+const Employee = require('./models/employees');
+const app = express();
+mongoose.set('strictQuery', false);
 
-console.log(uuidv4());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const employ = [
+    {
+        "name": "Emmanuel",
+        "industry": "Music"
+    },
+    {
+        "name": "Peter",
+        "industry": "Preacher"
+    },
+    {
+        "name": "Paul",
+        "industry": "Communication"
+    }
+];
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const PORT = process.env.PORT || 3004;
+const CONNECTION = process.env.CONNECTION;
+
+const employee = new Employee({
+    name: 'Joseph',
+    industry: 'Marketing'
+});
+
+employee.save();
+
+app.get('/', (req, res) => {
+    res.send(employee);
+});
+
+app.get('/api/employee', (req, res) => {
+    console.log(req.body)
+    res.send(req.body);
+});
+
+// Connect with mongoose
+
+const start = async () => {
+    try {
+
+        await mongoose.connect(CONNECTION);
+    
+        app.listen(PORT, () => {
+            console.log(`App running on... ${PORT}`);
+        });
+
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
